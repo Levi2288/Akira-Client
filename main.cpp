182d747eb842712dfa372c85880153d23cbd58ce
Levi2288 <54726665+Levi2288@users.noreply.github.com>
2023-01-10 17:18:54 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 #include <string>
 #include <iostream>
 
+std::string Moofy = "ADHD";
+
+bool Moofy_Moof()
+{
+  
+  if(Moofy == "ADHD")
+    return true;
+  return false;
+}
+
+
 int main(){
 
-  std::cout << "Stop Pasting Dumbass << std::endl;
+  std::cout << "Stop Pasting Dumbass" << std::endl;
+  if(Moofy_Moof())
+    std::cout << "Penis! :3" << std::endl;
 }
+
